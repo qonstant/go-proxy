efdc3e299bdc3ca89dac8b3cea0fb67259032cef
Rakymzhan Zhabagin <rakorako2204@gmail.com>
2024-07-15 09:53:28 +0500

    refactoring

--- docs/docs.go
+++ docs/docs.go
@@ -118,7 +118,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "go-proxy-1fo6.onrender.com",
 	BasePath:         "/",
 	Schemes:          []string{"http", "https"},
 	Title:            "HTTP Proxy Server API",
